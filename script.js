f630b84b37d0ce9d2dd0ec97c05a09693af99940
Trevor Villwock <trevorvillwock@gmail.com>
2023-02-22 15:42:28 -0600

    loads local audio file

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function closeModal() {
     modal.style.display="none";
     Tone.start();
 }
+
+function handleFile() {
+    let file = document.querySelector('input[type=file]').files[0];
+    let audioURL = URL.createObjectURL(file);
+    player.load(audioURL);
+}
+
+/* Method of loading audio files adapted from these links:
+https://codepen.io/ankerpeet/pen/GQRdLK
+https://stackoverflow.com/questions/70910548/play-loaded-audio-file-with-tone-js-web-audio-framework
+https://editor.p5js.org/llelell/sketches/3icSjW6xQ
+*/
