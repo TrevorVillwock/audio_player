4adb454b014fbcefdd7467476b2e0772d3b7e24e
Trevor Villwock <trevorvillwock@gmail.com>
2023-02-24 09:00:15 -0600

    working prototype

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-const player =new Tone.Player("https://tonejs.github.io/audio/berklee/gong_1.mp3").toDestination();
-player.loop = true;
+const player = new Tone.Player().toDestination();
+player.playbackRate = 1;
+// player.loop = true;
+
+// Array to hold all audio files uploaded by user
+let audioFiles = [];
+
+// Position in array of next loaded file
+let loadFileNum = 0;
+
+// Position of next file to play in array
+let playFileNum = 0;
+
+// Flag signifying whether app is playing or not
+let isPlaying = 0;
+
+// Without async/await here, the player starts before the 
+// audio file has finished loading
+player.onstop = async () => {
+    console.log(`playFileNum before: ${playFileNum}`);
+    if (isPlaying) {
+        if (playFileNum < loadFileNum - 1) {
+            playFileNum++;
+        } else {
+            playFileNum = 0;
+        }
+        await player.load(audioFiles[playFileNum]);
+        player.start();
+    }
+    console.log(`playFileNum after: ${playFileNum}`);
+}
 
 function start() {
     player.start();
+    isPlaying = 1;
 }
 
 function stop() {
     player.stop();
+    playFileNum = 0;
+    isPlaying = 0;
 }
 
 function closeModal() {
@@ -15,10 +47,14 @@ function closeModal() {
     Tone.start();
 }
 
-function handleFile() {
-    let file = document.querySelector('input[type=file]').files[0];
-    let audioURL = URL.createObjectURL(file);
-    player.load(audioURL);
+function handleFiles() {
+    let files = document.getElementById("file-input").files;
+    for (let i = 0; i < files.length; i++) {
+        let audioURL = URL.createObjectURL(files[i]);
+        audioFiles[loadFileNum] = audioURL;
+        loadFileNum++;
+    }
+    player.load(audioFiles[0]);
 }
 
 /* Method of loading audio files adapted from these links:
